dc58c231431440ab0dd958aabd521b52aff935b5
fancy <274737258@qq.com>
2019-08-21 10:10:36 +0800

    mvvm代码优化

--- app/src/main/java/com/fancy/mvvmdemo/UserActivity.java
+++ app/src/main/java/com/fancy/mvvmdemo/UserActivity.java
@@ -4,22 +4,22 @@ import android.arch.lifecycle.Observer;
 import android.support.annotation.Nullable;
 
 import com.fancy.mvvmdemo.bean.User;
-import com.fancy.mvvmdemo.databinding.ActivityLoginBinding;
+import com.fancy.mvvmdemo.databinding.ActivityUserBinding;
 import com.fancy.mvvmdemo.model.UserViewModel;
 
 /**
  * @author pengkuanwang
  * @date 2019-08-20
  */
-public class LoginActivity extends BaseActivity<ActivityLoginBinding, UserViewModel> {
+public class UserActivity extends BaseActivity<ActivityUserBinding, UserViewModel> {
     @Override
     protected int initContentView() {
-        return R.layout.activity_login;
+        return R.layout.activity_user;
     }
 
     @Override
     protected void initData() {
-        viewModel.getUser("我是你大爷啊", "11111男").observe(this, new Observer<User>() {
+        viewModel.getUser("name", "gender").observe(this, new Observer<User>() {
             @Override
             public void onChanged(@Nullable User user) {
                 binding.setUser(user);
--- app/src/main/java/com/fancy/mvvmdemo/util/UserRepository.java
+++ app/src/main/java/com/fancy/mvvmdemo/util/UserRepository.java
@@ -2,6 +2,9 @@ package com.fancy.mvvmdemo.util;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
 
 import com.fancy.mvvmdemo.bean.HttpResult;
 import com.fancy.mvvmdemo.bean.User;
@@ -18,6 +21,8 @@ import io.reactivex.Observable;
  */
 public class UserRepository {
     private static final UserRepository instance = new UserRepository();
+    private Handler handler = new Handler(Looper.getMainLooper());
+
 
     private UserRepository() {
     }
@@ -26,11 +31,25 @@ public class UserRepository {
         return instance;
     }
 
-    public LiveData<User> getUser(String userName, String gender) {
+    public LiveData<User> getUser(final String userName, final String gender) {
+        final MutableLiveData<User> mutableLiveData = new MutableLiveData<>();
         Observable<HttpResult<UserBean>> observable = Api.getDefault().login(Util.getRequest(new HashMap<String, String>()));
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(1000);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mutableLiveData.setValue(new User("彭宽旺", "男"));
+                    }
+                });
+            }
+        }).start();
         /*HttpUtil.getInstance().toSubscribe(observable, new ProgressSubscriber<User>() {
             @Override
             protected void _onNext(User user) {
+                mutableLiveData.setValue(user);
             }
 
             @Override
@@ -38,8 +57,6 @@ public class UserRepository {
 
             }
         });*/
-        MutableLiveData<User> user = new MutableLiveData<>();
-        user.setValue(new User(userName, gender));
-        return user;
+        return mutableLiveData;
     }
 }
