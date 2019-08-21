fabac4a7e52c8c89927473848d35a1eb799b6303
fancy <274737258@qq.com>
2019-08-21 11:24:03 +0800

    mvvm代码优化

--- app/src/main/java/com/fancy/mvvmdemo/model/LoginViewModel.java
+++ app/src/main/java/com/fancy/mvvmdemo/model/LoginViewModel.java
@@ -1,8 +1,6 @@
 package com.fancy.mvvmdemo.model;
 
-import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
-import android.support.annotation.NonNull;
+import android.arch.lifecycle.ViewModel;
 
 import com.fancy.mvvmdemo.util.ToastUtil;
 import com.fancy.mvvmdemo.view.BindingAction;
@@ -12,17 +10,13 @@ import com.fancy.mvvmdemo.view.BindingCommand;
  * @author pengkuanwang
  * @date 2019-08-21
  */
-public class LoginViewModel extends AndroidViewModel {
+public class LoginViewModel extends ViewModel {
     private BindingCommand loginOnClickCommand = new BindingCommand(new BindingAction() {
         @Override
         public void call() {
             login();
         }
     });
-
-    public LoginViewModel(@NonNull Application application) {
-        super(application);
-    }
 
     private void login() {
         ToastUtil.showCustomToast("登录了啊");
