3489ab55794868d9378d9f142c0ee4450a2751c3
fancy <274737258@qq.com>
2019-08-26 17:46:18 +0800

    定义首页tab

--- app/src/main/java/com/fancy/mvvmdemo/fragment/BaseFragment.java
+++ app/src/main/java/com/fancy/mvvmdemo/fragment/BaseFragment.java
@@ -1,10 +1,33 @@
 package com.fancy.mvvmdemo.fragment;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fancy.mvvmdemo.BaseViewModel;
 import com.trello.rxlifecycle2.components.RxFragment;
 
 /**
  * @author pengkuanwang
  * @date 2019-08-26
  */
-public class BaseFragment extends RxFragment {
+public abstract class BaseFragment<V extends ViewDataBinding, VM extends BaseViewModel> extends RxFragment {
+    protected V binding;
+    protected VM viewModel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, initContentView(), container, false);
+        return binding.getRoot();
+    }
+
+    /**
+     * 初始化根布局
+     *
+     * @return 布局layout的id
+     */
+    public abstract int initContentView();
 }
--- app/src/main/java/com/fancy/mvvmdemo/fragment/DiscoverFragment.java
+++ app/src/main/java/com/fancy/mvvmdemo/fragment/DiscoverFragment.java
@@ -1,8 +1,14 @@
 package com.fancy.mvvmdemo.fragment;
 
+import com.fancy.mvvmdemo.R;
+
 /**
  * @author pengkuanwang
  * @date 2019-08-26
  */
 public class DiscoverFragment extends BaseFragment{
+    @Override
+    public int initContentView() {
+        return R.layout.fragment_discover;
+    }
 }
